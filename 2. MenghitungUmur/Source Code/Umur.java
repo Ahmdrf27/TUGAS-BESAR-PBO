77097f1c3baf41cd2c0b804b8469b8ea5f010a7d
A. Rifai <87814697+Ahmdrf27@users.noreply.github.com>
2022-01-09 20:32:14 +0700

    Add files via upload

---
+++ 2. MenghitungUmur/Source Code/Umur.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package AplikasiMenghitungUmur;
+
+/**
+ *
+ * @author HP
+ */
+public class Umur {
+    public static void main(String args[]){
+        Masuk msk = new Masuk();
+        msk.setVisible(true);
+        msk.pack();
+        msk.setLocationRelativeTo(null);
+        msk.setDefaultCloseOperation(Masuk.EXIT_ON_CLOSE); 
+    }
+    
+}
