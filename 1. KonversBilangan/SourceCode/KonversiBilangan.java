fd53447a7ab64babc248bf0c7c19a14606ce1522
A. Rifai <87814697+Ahmdrf27@users.noreply.github.com>
2022-01-09 20:28:12 +0700

    Add files via upload

---
+++ 1. KonversBilangan/SourceCode/KonversiBilangan.java
@@ -0,0 +1,26 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package konversibilangan;
+
+/**
+ *
+ * @author HP
+ */
+public class KonversiBilangan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login lg = new Login();
+        lg.setVisible(true);
+        lg.pack();
+        lg.setLocationRelativeTo(null);
+        lg.setDefaultCloseOperation(Login.EXIT_ON_CLOSE);
+    }
+    
+}
